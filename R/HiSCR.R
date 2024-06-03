421d2fe95c735da7183d4fe21fdbcb8283cc980f
Bill Denney <wdenney@humanpredictions.com>
2024-06-03 10:56:00 -0400

    Allow NA values in calculate_hiscr

--- R/HiSCR.R
+++ R/HiSCR.R
@@ -8,9 +8,9 @@
 #' @param timepoint_fistula Integer vector representing the draining fistula count at the time point of interest.
 #' @param percentage Numeric value specifying the percentage required for HiSCR (e.g., 50 for HiSCR50, 75 for HiSCR75, etc.).
 #' @return A logical vector indicating whether the HiSCR response is achieved for each set of input parameters.
-#' @export 
+#' @export
 #' @examples
-#' 
+#'
 #' baseline_abscess <- c(3, 2, 4)
 #' baseline_nodule <- c(5, 4, 6)
 #' baseline_fistula <- c(2, 1, 3)
@@ -22,38 +22,37 @@
 #' hiscr_response <- calculate_hiscr(baseline_abscess, baseline_nodule, baseline_fistula,
 #'                                   timepoint_abscess, timepoint_nodule, timepoint_fistula,
 #'                                   percentage)
-#' 
 
 calculate_hiscr <- function(baseline_abscess, baseline_nodule, baseline_fistula,
                             timepoint_abscess, timepoint_nodule, timepoint_fistula,
                             percentage) {
-  
-  checkmate::assert_integerish(baseline_abscess, lower = 0, any.missing = FALSE, null.ok = FALSE)
-  checkmate::assert_integerish(baseline_nodule, lower = 0, len = length(baseline_abscess), any.missing = FALSE, null.ok = FALSE)
-  checkmate::assert_integerish(baseline_fistula, lower = 0, len = length(baseline_abscess), any.missing = FALSE, null.ok = FALSE)
-  checkmate::assert_integerish(timepoint_abscess, lower = 0, len = length(baseline_abscess), any.missing = FALSE, null.ok = FALSE)
-  checkmate::assert_integerish(timepoint_nodule, lower = 0, len = length(baseline_abscess), any.missing = FALSE, null.ok = FALSE)
-  checkmate::assert_integerish(timepoint_fistula, lower = 0, len = length(baseline_abscess), any.missing = FALSE, null.ok = FALSE)
+
+  checkmate::assert_integerish(baseline_abscess, lower = 0, null.ok = FALSE)
+  checkmate::assert_integerish(baseline_nodule, lower = 0, len = length(baseline_abscess), null.ok = FALSE)
+  checkmate::assert_integerish(baseline_fistula, lower = 0, len = length(baseline_abscess), null.ok = FALSE)
+  checkmate::assert_integerish(timepoint_abscess, lower = 0, len = length(baseline_abscess), null.ok = FALSE)
+  checkmate::assert_integerish(timepoint_nodule, lower = 0, len = length(baseline_abscess), null.ok = FALSE)
+  checkmate::assert_integerish(timepoint_fistula, lower = 0, len = length(baseline_abscess), null.ok = FALSE)
   checkmate::assert_number(percentage, lower = 50, upper = 100, null.ok = FALSE)
-  
+
   # Calculate total abscess and inflammatory nodule count at baseline and time point
   baseline_total <- baseline_abscess + baseline_nodule
   timepoint_total <- timepoint_abscess + timepoint_nodule
-  
+
   # Calculate the percentage reduction in total abscess and inflammatory nodule count
   reduction_percentage <- (baseline_total - timepoint_total) / baseline_total * 100
-  
+
   # Check if the redction percentage meets the specified threshold
   reduction_criteria <- reduction_percentage >= percentage
-  
+
   # Check if there is no increase in abscess count
   abscess_criteria <- timepoint_abscess <= baseline_abscess
-  
+
   # Check if there is no increase in draining fistula count
   fistula_criteria <- timepoint_fistula <= baseline_fistula
-  
+
   # Determine if HiSCR response is achieved based on all criteria
   hiscr_response <- reduction_criteria & abscess_criteria & fistula_criteria
-  
+
   hiscr_response
 }
