2420c5a16c681f47b093619f4f0f1e549813765b
Devon Bartlett <dbartlett@humanpredictions.com>
2024-06-07 17:07:06 -0500

    Properly name changing (hopefully) and adding a single test for setup of testing hasi.R

--- tests/testthat/test-hasi.R
+++ tests/testthat/test-hasi.R
@@ -1,3 +1,15 @@
-test_that("hs_hasi_r", {
-  expect_equal(2 * 2, 4)
+test_that("Simple case with one patient, one visit, and one body site", {
+  patientID <- c("000-001")
+  visitDY <- c(1)
+  BodySite <- c("Axillae")
+  BSA <- c(1)
+  InflammColorChg <- c(1)
+  Induration <- c(1)
+  OpenSkinSurface <- c(1)
+  Tunnels <- c(1)
+  
+  expected <- data.frame(patientID = "000-001", visitDY = 1, HASI_R_Score = 4)
+  result <- hasi_r_num(patientID, visitDY, BodySite, BSA, InflammColorChg, Induration, OpenSkinSurface, Tunnels)
+  
+  expect_equal(result, expected)
 })
