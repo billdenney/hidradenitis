94c985d4634a691b9d1d38fa6d28fdf06df6e1ca
Bill Denney <wdenney@humanpredictions.com>
2024-06-29 19:20:01 -0400

    Update docs, simplify example

--- R/HS_PGA_char.R
+++ R/HS_PGA_char.R
@@ -1,19 +1,16 @@
 #' Converts HS-PGA numerical scores to character values.
 #'
-#'
 #' @references Marzano, A V et al. “Creation of a severity index for
 #'   hidradenitis suppurativa that includes a validated quality-of-life
 #'   measure: the HIDRAscore.” Journal of the European Academy of Dermatology
 #'   and Venereology : JEADV vol. 34,8 (2020): 1815-1821. doi:10.1111/jdv.16328
 #'
-#' @param hs_pga_scores Integer vector representing the HS-PGA scores (1: Clean, 2: Minimal, 3: Mild, 4: Moderate, 5: Severe, 6: Very Severe).
+#' @param hs_pga_scores Integer vector representing the HS-PGA scores (1: Clean,
+#'   2: Minimal, 3: Mild, 4: Moderate, 5: Severe, 6: Very Severe).
 #' @return A character vector representing the corresponding HS-PGA categories.
 #' @export
 #' @examples
-#' hs_pga_scores <- c(1, 2, 3, 4, 5, 6)
-#'
-#' hs_pga_char(hs_pga_scores)
-#'
+#' hs_pga_char(c(1, 2, 3, 4, 5, 6))
 hs_pga_char <- function(hs_pga_scores) {
 
   # Assertions using checkmate package
