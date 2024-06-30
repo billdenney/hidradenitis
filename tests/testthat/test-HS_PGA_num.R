5872a65d5867f08010d8663149b2a990b1e1f0c2
Bill Denney <wdenney@humanpredictions.com>
2024-06-29 22:23:40 -0400

    Make tests more explicit

--- tests/testthat/test-HS_PGA_num.R
+++ tests/testthat/test-HS_PGA_num.R
@@ -1,82 +1,4 @@
 test_that("check_clean_score", {
-  expect_equal(hs_pga_num(0, 0, 0), 1)
-})
-
-test_that("check_minimal_score", {
-  expect_equal(hs_pga_num(0, 0, 1), 2)
-  expect_equal(hs_pga_num(0, 0, 5), 2)
-})
-
-test_that("check_mild_score", {
-  expect_equal(hs_pga_num(0, 1, 0), 3)
-  expect_equal(hs_pga_num(0, 4, 0), 3)
-  expect_equal(hs_pga_num(1, 0, 0), 3)
-})
-
-test_that("check_moderate_score", {
-  expect_equal(hs_pga_num(0, 5, 0), 4)
-  expect_equal(hs_pga_num(1, 1, 0), 4)
-  expect_equal(hs_pga_num(2, 0, 0), 4)
-  expect_equal(hs_pga_num(5, 9, 0), 4)
-})
-
-test_that("check_severe_scores", {
-  expect_equal(hs_pga_num(2, 10, 0), 5)
-  expect_equal(hs_pga_num(5, 15, 0), 5)
-})
-
-test_that("check_very_severe_score", {
-  expect_equal(hs_pga_num(6, 0, 0), 6)
-  expect_equal(hs_pga_num(10, 5, 0), 6)
-})
-
-test_that("check_input_length_consistency", {
-  expect_error(hs_pga_num(c(0, 0), c(0, 0, 0), c(0, 0)))
-})
-
-test_that("check_missing_values", {
-  expect_equal(
-    hs_pga_num(c(0, NA, 0), c(0, 0, 0), c(0, 0, 0)),
-    c(1, NA, 1)
-  )
-  expect_equal(
-    hs_pga_num(c(0, 0, 0), c(0, NA, 0), c(0, 0, 0)),
-    c(1, NA, 1)
-  )
-  expect_equal(
-    hs_pga_num(c(0, 0, 0), c(0, 0, 0), c(0, NA, 0)),
-    c(1, NA, 1)
-  )
-})
-
-test_that("check_negative_values", {
-  expect_error(hs_pga_num(c(-1, 0, 0), c(0, 0, 0), c(0, 0, 0)))
-  expect_error(hs_pga_num(c(0, 0, 0), c(-1, 0, 0), c(0, 0, 0)))
-  expect_error(hs_pga_num(c(0, 0, 0), c(0, 0, 0), c(-1, 0, 0)))
-})
-
-test_that("check_non_integer_values", {
-  expect_error(hs_pga_num(c(0.5, 0, 0), c(0, 0, 0), c(0, 0, 0)))
-  expect_error(hs_pga_num(c(0, 0, 0), c(0.5, 0, 0), c(0, 0, 0)))
-  expect_error(hs_pga_num(c(0, 0, 0), c(0, 0, 0), c(0.5, 0, 0)))
-})
-
-test_that("check_edge_cases", {
-  expect_equal(hs_pga_num(0, 0, 1), 2)
-  expect_equal(hs_pga_num(0, 1, 0), 3)
-  expect_equal(hs_pga_num(1, 0, 0), 3)
-  expect_equal(hs_pga_num(0, 5, 0), 4)
-  expect_equal(hs_pga_num(2, 9, 0), 4)
-  expect_equal(hs_pga_num(2, 10, 0), 5)
-  expect_equal(hs_pga_num(6, 0, 0), 6)
-})
-
-test_that("check_large_input_values", {
-  expect_equal(hs_pga_num(10, 20, 30), 6)
-  expect_equal(hs_pga_num(0, 100, 200), 4)
-})
-
-test_that("check_named_input_vectors", {
   expect_equal(
     hs_pga_num(
       abscess_fistula = 0,
@@ -84,6 +6,43 @@ test_that("check_named_input_vectors", {
       non_inflammatory_nodule = 0
     ),
     1
+  )
+})
+
+test_that("check_minimal_score", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 0,
+      non_inflammatory_nodule = 1
+    ),
+    2
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 0,
+      non_inflammatory_nodule = 5
+    ),
+    2
+  )
+})
+
+test_that("check_mild_score", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 1,
+      non_inflammatory_nodule = 0),
+    3
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 4,
+      non_inflammatory_nodule = 0
+    ),
+    3
   )
   expect_equal(
     hs_pga_num(
@@ -95,12 +54,258 @@ test_that("check_named_input_vectors", {
   )
 })
 
-test_that("check_character_input", {
-  expect_error(hs_pga_num("0", "0", "0"))
-  expect_error(hs_pga_num("1", "0", "0"))
+test_that("check_moderate_score", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 5,
+      non_inflammatory_nodule = 0
+    ),
+    4
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 1,
+      inflammatory_nodule = 1,
+      non_inflammatory_nodule = 0
+    ),
+    4
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 2,
+      inflammatory_nodule = 0,
+      non_inflammatory_nodule = 0
+    ),
+    4
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 5,
+      inflammatory_nodule = 9,
+      non_inflammatory_nodule = 0
+    ),
+    4
+  )
 })
 
-test_that("check_logical_input", {
-  expect_error(hs_pga_num(FALSE, FALSE, FALSE))
-  expect_error(hs_pga_num(TRUE, FALSE, FALSE))
+test_that("check_severe_scores", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 2,
+      inflammatory_nodule = 10,
+      non_inflammatory_nodule = 0
+    ),
+    5
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 5,
+      inflammatory_nodule = 15,
+      non_inflammatory_nodule = 0
+    ),
+    5
+  )
+})
+
+test_that("check_very_severe_score", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 6,
+      inflammatory_nodule = 0,
+      non_inflammatory_nodule = 0
+    ),
+    6
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 10,
+      inflammatory_nodule = 5,
+      non_inflammatory_nodule = 0
+    ),
+    6
+  )
+})
+
+test_that("check_input_length_consistency", {
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0, 0)
+    ),
+    regexp = "'inflammatory_nodule'.*Must have length 2, but has length 3."
+  )
+})
+
+test_that("check_missing_values", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = c(0, NA, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    c(1, NA, 1)
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(0, NA, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    c(1, NA, 1)
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0, NA, 0)
+    ),
+    c(1, NA, 1)
+  )
+})
+
+test_that("check_negative_values", {
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(-1, 0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    regexp = "'abscess_fistula' failed: Element 1 is not >= 0."
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(-1, 0, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    regexp = "'inflammatory_nodule' failed: Element 1 is not >= 0."
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(-1, 0, 0)
+    ),
+    regexp = "'non_inflammatory_nodule' failed: Element 1 is not >= 0."
+  )
+})
+
+test_that("check_non_integer_values", {
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0.5, 0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    regexp = "'abscess_fistula' failed: Must be of type 'integerish'"
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(0.5, 0, 0),
+      non_inflammatory_nodule = c(0, 0, 0)
+    ),
+    regexp = "'inflammatory_nodule' failed: Must be of type 'integerish'"
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = c(0, 0, 0),
+      inflammatory_nodule = c(0, 0, 0),
+      non_inflammatory_nodule = c(0.5, 0, 0)
+    ),
+    regexp = "'non_inflammatory_nodule' failed: Must be of type 'integerish'"
+  )
+})
+
+test_that("check_edge_cases", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0, inflammatory_nodule = 0, non_inflammatory_nodule = 1
+    ),
+    2
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0, inflammatory_nodule = 1, non_inflammatory_nodule = 0
+    ),
+    3
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 1, inflammatory_nodule = 0, non_inflammatory_nodule = 0
+    ),
+    3
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0, inflammatory_nodule = 5, non_inflammatory_nodule = 0
+    ),
+    4
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 2, inflammatory_nodule = 9, non_inflammatory_nodule = 0
+    ),
+    4
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 2, inflammatory_nodule = 10, non_inflammatory_nodule = 0
+    ),
+    5
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 6, inflammatory_nodule = 0, non_inflammatory_nodule = 0
+    ),
+    6
+  )
+})
+
+test_that("check_large_input_values", {
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 10,
+      inflammatory_nodule = 20,
+      non_inflammatory_nodule = 30
+    ),
+    6
+  )
+  expect_equal(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 100,
+      non_inflammatory_nodule = 200
+    ),
+    4
+  )
+})
+
+test_that("check_character_input", {
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = "0",
+      inflammatory_nodule = "0",
+      non_inflammatory_nodule = "0"
+    ),
+    regexp = "'abscess_fistula' failed: Must be of type 'integerish'"
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = "0",
+      non_inflammatory_nodule = "0"
+    ),
+    regexp = "'inflammatory_nodule' failed: Must be of type 'integerish'"
+  )
+  expect_error(
+    hs_pga_num(
+      abscess_fistula = 0,
+      inflammatory_nodule = 0,
+      non_inflammatory_nodule = "0"
+    ),
+    regexp = "'non_inflammatory_nodule' failed: Must be of type 'integerish'"
+  )
 })
--- tests/testthat/test-HiSCR.R
+++ tests/testthat/test-HiSCR.R
@@ -1,19 +1,31 @@
 test_that("check_abscess_count_increase", {
-  expect_false(all(
-    hiscr(c(3, 2, 4), c(5, 4, 6), c(2, 1, 3),
-          c(4, 1, 2), c(2, 3, 2), c(2, 1, 3),
-          50)
-  ))
+  expect_equal(
+    hiscr(
+      baseline_abscess = c(3, 2, 4),
+      baseline_nodule = c(5, 4, 6),
+      baseline_fistula = c(2, 1, 3),
+      timepoint_abscess = c(4, 1, 2),
+      timepoint_nodule = c(2, 3, 2),
+      timepoint_fistula = c(2, 1, 3),
+      percentage = 50
+    ),
+    c(FALSE, FALSE, TRUE)
+  )
 })
 
 test_that("check_fistula_count_increase", {
-  expect_false(all(
+  expect_equal(
     hiscr(
-      c(3, 2, 4), c(5, 4, 6), c(2, 1, 3),
-      c(1, 1, 2), c(2, 3, 2), c(3, 2, 4),
-      50
-    )
-  ))
+      baseline_abscess = c(3, 2, 4),
+      baseline_nodule = c(5, 4, 6),
+      baseline_fistula = c(2, 1, 3),
+      timepoint_abscess = c(1, 1, 2),
+      timepoint_nodule = c(2, 3, 2),
+      timepoint_fistula = c(3, 2, 4),
+      percentage = 50
+    ),
+    c(FALSE, FALSE, FALSE)
+  )
 })
 
 test_that("check_input_length_consistency", {
@@ -22,60 +34,137 @@ test_that("check_input_length_consistency", {
       c(3, 2), c(5, 4, 6), c(2, 1, 3),
       c(1, 1, 2), c(2, 3, 2), c(2, 1, 3),
       50
-    )
+    ),
+    regexp = "'baseline_nodule' failed: Must have length 2, but has length 3."
   )
 })
 
 test_that("check_percentage_range", {
   expect_error(
     hiscr(
-      c(3, 2, 4), c(5, 4, 6), c(2, 1, 3),
-      c(1, 1, 2), c(2, 3, 2), c(2, 1, 3),
-      120
-    )
+      baseline_abscess = c(3, 2, 4),
+      baseline_nodule = c(5, 4, 6),
+      baseline_fistula = c(2, 1, 3),
+      timepoint_abscess = c(1, 1, 2),
+      timepoint_nodule = c(2, 3, 2),
+      timepoint_fistula = c(2, 1, 3),
+      percentage = 120
+    ),
+    regexp = "Assertion on 'percentage' failed: Element 1 is not <= 100."
   )
   expect_error(
     hiscr(
-      c(3, 2, 4), c(5, 4, 6), c(2, 1, 3),
-      c(1, 1, 2), c(2, 3, 2), c(2, 1, 3),
-      30
-    )
+      baseline_abscess = c(3, 2, 4),
+      baseline_nodule = c(5, 4, 6),
+      baseline_fistula = c(2, 1, 3),
+      timepoint_abscess = c(1, 1, 2),
+      timepoint_nodule = c(2, 3, 2),
+      timepoint_fistula = c(2, 1, 3),
+      percentage = 30
+    ),
+    regexp = "Assertion on 'percentage' failed: Element 1 is not >= 50"
   )
 })
 
 test_that("check_hiscr50_response", {
-  expect_true(hiscr(3, 5, 2, 1, 2, 2, 50))
-  expect_false(hiscr(3, 5, 2, 2, 3, 2, 50))
+  expect_true(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 1, timepoint_nodule = 2, timepoint_fistula = 2,
+      percentage = 50
+    )
+  )
+  expect_false(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 2, timepoint_nodule = 3, timepoint_fistula = 2,
+      percentage = 50
+    )
+  )
 })
 
 test_that("check_hiscr75_response", {
-  expect_true(hiscr(4, 6, 3, 1, 1, 3, 75))
-  expect_false(hiscr(4, 6, 3, 2, 2, 3, 75))
+  expect_true(
+    hiscr(
+      baseline_abscess = 4, baseline_nodule = 6, baseline_fistula = 3,
+      timepoint_abscess = 1, timepoint_nodule = 1, timepoint_fistula = 3,
+      percentage = 75
+    )
+  )
+  expect_false(
+    hiscr(
+      baseline_abscess = 4, baseline_nodule = 6, baseline_fistula = 3,
+      timepoint_abscess = 2, timepoint_nodule = 2, timepoint_fistula = 3,
+      percentage = 75
+    )
+  )
 })
 
 test_that("check_hiscr90_response", {
-  expect_true(hiscr(4, 6, 3, 1, 0, 0, 90))
-  expect_false(hiscr(4, 6, 3, 2, 2, 3, 90))
+  expect_true(
+    hiscr(
+      baseline_abscess = 4, baseline_nodule = 6, baseline_fistula = 3,
+      timepoint_abscess = 1, timepoint_nodule = 0, timepoint_fistula = 0,
+      percentage = 90
+    )
+  )
+  expect_false(
+    hiscr(
+      baseline_abscess = 4, baseline_nodule = 6, baseline_fistula = 3,
+      timepoint_abscess = 2, timepoint_nodule = 2, timepoint_fistula = 3,
+      percentage = 90
+    )
+  )
 })
 
 test_that("check_no_abscess_nodule_reduction", {
-  expect_false(hiscr(3, 5, 2, 3, 5, 2, 50))
+  expect_false(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 3, timepoint_nodule = 5, timepoint_fistula = 2,
+      percentage = 50
+    )
+  )
 })
 
 test_that("check_abscess_increase_nodule_decrease", {
-  expect_false(hiscr(3, 5, 2, 4, 2, 2, 50))
+  expect_false(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 4, timepoint_nodule = 2, timepoint_fistula = 2,
+      percentage = 50
+    )
+  )
 })
 
 test_that("check_nodule_increase_abscess_decrease", {
-  expect_false(hiscr(3, 5, 2, 1, 8, 2, 50))
+  expect_false(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 1, timepoint_nodule = 8, timepoint_fistula = 2,
+      percentage = 50
+    )
+  )
 })
 
 test_that("check_fistula_increase", {
-  expect_false(hiscr(3, 5, 2, 1, 2, 3, 50))
+  expect_false(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 1, timepoint_nodule = 2, timepoint_fistula = 3,
+      percentage = 50
+    )
+  )
 })
 
 test_that("check_complete_clearance", {
-  expect_true(hiscr(3, 5, 2, 0, 0, 0, 50))
+  expect_true(
+    hiscr(
+      baseline_abscess = 3, baseline_nodule = 5, baseline_fistula = 2,
+      timepoint_abscess = 0, timepoint_nodule = 0, timepoint_fistula = 0,
+      percentage = 50
+    )
+  )
 })
 
 test_that("hiscr NA values", {
